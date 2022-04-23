e381c51761e2299716bcdd48a7d5ca432192668d
bauda04 <antenainarandrianantoandro@gmail.com>
2022-04-23 03:15:51 +0300

    🚧 Displaying and Order Page using Cart Service (Front)

---
+++ frontend/src/app/order-page/dist/order-page.component.js
@@ -0,0 +1,45 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.OrderPageComponent = void 0;
+var core_1 = require("@angular/core");
+var OrderPageComponent = /** @class */ (function () {
+    function OrderPageComponent(cartService) {
+        this.cartService = cartService;
+        this.panier = localStorage.getItem('panier');
+    }
+    OrderPageComponent.prototype.ngOnInit = function () {
+        this.obj = JSON.parse(String(this.panier));
+        console.log(this.obj);
+        this.sum();
+    };
+    // removeToCart(dishes: Dishes) {
+    //   this.cartService.removeToCart(dishes);
+    //   this.sum();
+    //   window.alert(dishes.dishes_name + ' supprimé du panier!');
+    // }
+    OrderPageComponent.prototype.sum = function () {
+        var _this = this;
+        this.totalPrice = 0;
+        if (this.obj) {
+            this.obj.map(function (_dishes) {
+                var price = _dishes.dishes_price || 0;
+                _this.totalPrice += price;
+            });
+        }
+    };
+    OrderPageComponent = __decorate([
+        core_1.Component({
+            selector: 'app-order-page',
+            templateUrl: './order-page.component.html',
+            styleUrls: ['./order-page.component.css']
+        })
+    ], OrderPageComponent);
+    return OrderPageComponent;
+}());
+exports.OrderPageComponent = OrderPageComponent;
--- frontend/src/app/order-page/order-page.component.ts
+++ frontend/src/app/order-page/order-page.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Dishes } from '../shared/dishes.model';
 
 @Component({
   selector: 'app-order-page',
   templateUrl: './order-page.component.html',
-  styleUrls: ['./order-page.component.css']
+  styleUrls: ['./order-page.component.css'],
 })
 export class OrderPageComponent implements OnInit {
+  // items = this.cartService.getItems();
+  totalPrice!: number;
+  obj!: any;
 
-  constructor() { }
+  panier = localStorage.getItem('panier');
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
+    this.obj = JSON.parse(String(this.panier));
+    console.log(this.obj);
+    this.sum();
   }
 
+  // removeToCart(dishes: Dishes) {
+  //   this.cartService.removeToCart(dishes);
+  //   this.sum();
+  //   window.alert(dishes.dishes_name + ' supprimé du panier!');
+  // }
+
+  sum(): void {
+    this.totalPrice = 0;
+    if (this.obj) {
+      this.obj.map((_dishes:any) => {
+        const price: number = _dishes.dishes_price || 0;
+        this.totalPrice += price;
+      });
+    }
+  }
 }
