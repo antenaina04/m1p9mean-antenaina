aa0a97a8dc30c0c45a0e51ef7572d3c9460db745
bauda04 <antenainarandrianantoandro@gmail.com>
2022-05-01 17:51:08 +0300

    🔨 Add Save OrderDetails on saving Order in orderController.js - Back

--- backend/controllers/dist/orderController.dev.js
+++ backend/controllers/dist/orderController.dev.js
@@ -7,7 +7,10 @@ var router = express.Router();
 var ObjectId = require('mongoose').Types.ObjectId;
 
 var _require = require('../models/order'),
-    Order = _require.Order; // => localhost:3000/order/
+    Order = _require.Order;
+
+var _require2 = require('../models/orderDetails'),
+    OrderDetail = _require2.OrderDetail; // => localhost:3000/order/
 
 
 router.get('/', function (req, res) {
@@ -46,53 +49,79 @@ router.get('/GetOrderByIdUserAndStatus/:id_user/:order_status', function (req, r
   });
 }); // => localhost:3000/order/
 
-router.post('/', function (req, res) {
-  var order = new Order({
-    created_at: null,
-    updated_at: null,
-    order_price: req.body.order_price,
-    order_status: req.body.order_status,
-    id_user: req.body.id_user
-  });
-  order.save(function (err, docs) {
-    if (!err) {
-      res.send(docs);
-    } else {
-      console.log('Error in Order Save :' + JSON.stringify(err, undefined, 2));
+router.post('/', function _callee(req, res) {
+  var order, OrderSave;
+  return regeneratorRuntime.async(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          order = new Order({
+            created_at: null,
+            updated_at: null,
+            order_price: req.body.order_price,
+            order_status: req.body.order_status,
+            id_user: req.body.id_user
+          });
+          _context.next = 3;
+          return regeneratorRuntime.awrap(order.save(function (err, docs) {
+            if (!err) {
+              //Save Order Details
+              console.log("Id Order in docs =" + docs._id); //Id Order Last Created
+
+              for (var i = 0; i < JSON.parse(req.body.cart).length; i++) {
+                // console.log(JSON.parse(req.body.cart)[i]._id);
+                var orderDetail = new OrderDetail({
+                  created_at: null,
+                  updated_at: null,
+                  quantity: 0,
+                  //Unfinished
+                  id_order: docs._id,
+                  //OrderId
+                  id_dishes: JSON.parse(req.body.cart)[i]._id // IdDishes
+
+                });
+                var OrderDetailSave = orderDetail.save(function (err, docs) {});
+              }
+
+              res.send(docs);
+            } else {
+              console.log('Error in Order Save :' + JSON.stringify(err, undefined, 2));
+            }
+          }));
+
+        case 3:
+          OrderSave = _context.sent;
+
+        case 4:
+        case "end":
+          return _context.stop();
+      }
     }
   });
 }); // => localhost:3000/order/_id
+// router.put('/:id', (req, res) => {
+//     if (!ObjectId.isValid(req.params.id))
+//         return res.status(400).send(`No record with given id : ${req.params.id}`);
+//     var order = new Order({
+//         created_at: null,
+//         updated_at: null,
+//         order_price: req.body.order_price,
+//         order_status: req.body.order_status,
+//         id_user: req.body.id_user,
+//     });
+//     Order.findByIdAndUpdate(req.params.id, { $set: order }, { new: true }, (err, doc) => {
+//         if (!err) { res.send(doc); }
+//         else { console.log('Error in Order Update :' + JSON.stringify(err, undefined, 2)); }
+//     });
+// });
+// => localhost:3000/order/_id
+// router.delete('/:id', (req, res) => {
+//     if (!ObjectId.isValid(req.params.id))
+//         return res.status(400).send(`No record with given id : ${req.params.id}`);
+//     Order.findByIdAndRemove(req.params.id, (err, doc) => {
+//         if (!err) { res.send(doc); }
+//         else { console.log('Error in Order Delete :' + JSON.stringify(err, undefined, 2)); }
+//     });
+// });
 
-router.put('/:id', function (req, res) {
-  if (!ObjectId.isValid(req.params.id)) return res.status(400).send("No record with given id : ".concat(req.params.id));
-  var order = new Order({
-    created_at: null,
-    updated_at: null,
-    order_price: req.body.order_price,
-    order_status: req.body.order_status,
-    id_user: req.body.id_user
-  });
-  Order.findByIdAndUpdate(req.params.id, {
-    $set: order
-  }, {
-    "new": true
-  }, function (err, doc) {
-    if (!err) {
-      res.send(doc);
-    } else {
-      console.log('Error in Order Update :' + JSON.stringify(err, undefined, 2));
-    }
-  });
-}); // => localhost:3000/order/_id
-
-router["delete"]('/:id', function (req, res) {
-  if (!ObjectId.isValid(req.params.id)) return res.status(400).send("No record with given id : ".concat(req.params.id));
-  Order.findByIdAndRemove(req.params.id, function (err, doc) {
-    if (!err) {
-      res.send(doc);
-    } else {
-      console.log('Error in Order Delete :' + JSON.stringify(err, undefined, 2));
-    }
-  });
-});
 module.exports = router;
--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -4,6 +4,8 @@ var ObjectId = require('mongoose').Types.ObjectId;
 
 
 var { Order } = require('../models/order');
+var { OrderDetail } = require('../models/orderDetails');
+
 
 // => localhost:3000/order/
 router.get('/', (req, res) => {
@@ -38,7 +40,7 @@ router.get('/GetOrderByIdUserAndStatus/:id_user/:order_status', (req, res) => {
 });
 
 // => localhost:3000/order/
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     var order = new Order({
         created_at: null,
         updated_at: null,
@@ -47,41 +49,59 @@ router.post('/', (req, res) => {
         id_user: req.body.id_user,
     });
 
-    order.save((err, docs) => {
-        if (!err) { res.send(docs); }
+    var OrderSave = await order.save((err, docs) => {
+        if (!err) {
+            //Save Order Details
+            console.log("Id Order in docs =" + docs._id); //Id Order Last Created
+            for (var i = 0; i < JSON.parse(req.body.cart).length; i++) {
+                // console.log(JSON.parse(req.body.cart)[i]._id);
+                var orderDetail = new OrderDetail({
+                    created_at: null,
+                    updated_at: null,
+                    quantity: 0, //Unfinished
+                    id_order: docs._id, //OrderId
+                    id_dishes: JSON.parse(req.body.cart)[i]._id, // IdDishes
+                });
+                var OrderDetailSave = orderDetail.save((err, docs) => {
+                });
+            }
+            res.send(docs);
+        }
         else { console.log('Error in Order Save :' + JSON.stringify(err, undefined, 2)); }
     });
 });
 
-// => localhost:3000/order/_id
-router.put('/:id', (req, res) => {
-    if (!ObjectId.isValid(req.params.id))
-        return res.status(400).send(`No record with given id : ${req.params.id}`);
 
-    var order = new Order({
-        created_at: null,
-        updated_at: null,
-        order_price: req.body.order_price,
-        order_status: req.body.order_status,
-        id_user: req.body.id_user,
-    });
-
-    Order.findByIdAndUpdate(req.params.id, { $set: order }, { new: true }, (err, doc) => {
-        if (!err) { res.send(doc); }
-        else { console.log('Error in Order Update :' + JSON.stringify(err, undefined, 2)); }
-    });
-});
 
 // => localhost:3000/order/_id
-router.delete('/:id', (req, res) => {
-    if (!ObjectId.isValid(req.params.id))
-        return res.status(400).send(`No record with given id : ${req.params.id}`);
+// router.put('/:id', (req, res) => {
+//     if (!ObjectId.isValid(req.params.id))
+//         return res.status(400).send(`No record with given id : ${req.params.id}`);
 
-    Order.findByIdAndRemove(req.params.id, (err, doc) => {
-        if (!err) { res.send(doc); }
-        else { console.log('Error in Order Delete :' + JSON.stringify(err, undefined, 2)); }
-    });
-});
+//     var order = new Order({
+//         created_at: null,
+//         updated_at: null,
+//         order_price: req.body.order_price,
+//         order_status: req.body.order_status,
+//         id_user: req.body.id_user,
+//     });
+
+//     Order.findByIdAndUpdate(req.params.id, { $set: order }, { new: true }, (err, doc) => {
+//         if (!err) { res.send(doc); }
+//         else { console.log('Error in Order Update :' + JSON.stringify(err, undefined, 2)); }
+//     });
+// });
+
+// => localhost:3000/order/_id
+// router.delete('/:id', (req, res) => {
+//     if (!ObjectId.isValid(req.params.id))
+//         return res.status(400).send(`No record with given id : ${req.params.id}`);
+
+//     Order.findByIdAndRemove(req.params.id, (err, doc) => {
+//         if (!err) { res.send(doc); }
+//         else { console.log('Error in Order Delete :' + JSON.stringify(err, undefined, 2)); }
+//     });
+// });
 
 
 module.exports = router;
