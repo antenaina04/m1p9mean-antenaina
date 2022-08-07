302eeee36428d9f53103a0bd595ee742a971a7f0
bauda04 <antenainarandrianantoandro@gmail.com>
2022-08-07 17:52:28 +0300

    ➕ Update Order Status By Delivery - Frontend

--- frontend/src/app/get-all-deliveries/dist/get-all-deliveries.component.js
+++ frontend/src/app/get-all-deliveries/dist/get-all-deliveries.component.js
@@ -9,8 +9,11 @@ exports.__esModule = true;
 exports.GetAllDeliveriesComponent = void 0;
 var core_1 = require("@angular/core");
 var GetAllDeliveriesComponent = /** @class */ (function () {
-    function GetAllDeliveriesComponent(deliveryService) {
+    function GetAllDeliveriesComponent(_Activatedroute, deliveryService, _router) {
+        this._Activatedroute = _Activatedroute;
         this.deliveryService = deliveryService;
+        this._router = _router;
+        this.IdDeliverer = localStorage.getItem('IdDeliverer');
     }
     GetAllDeliveriesComponent.prototype.ngOnInit = function () {
         this.GetOrderDeliveryList();
@@ -19,7 +22,38 @@ var GetAllDeliveriesComponent = /** @class */ (function () {
         var _this = this;
         this.deliveryService.GetOrderDeliveryList().subscribe(function (res) {
             _this.deliveryService.delivery = res;
-            console.log("DeliveryList =" + _this.deliveryService.delivery);
+            console.log('DeliveryList =' + _this.deliveryService.delivery);
+        });
+    };
+    GetAllDeliveriesComponent.prototype.onSubmit = function (delivery) {
+        // Esorina ny []
+        var objIdDeliverer = String(this.IdDeliverer).replace('[', '');
+        var strIdDeliverer = objIdDeliverer.replace('"', '');
+        var lastRemovedCharStrIdDeliverer = strIdDeliverer.replace(']', '');
+        this.newStrIdDeliverer = lastRemovedCharStrIdDeliverer.replace('"', '');
+        var data = {
+            delivery_deliverer: this.newStrIdDeliverer
+        };
+        this.deliveryService
+            .putDelivery(data, String(delivery._id))
+            .subscribe(function (res) {
+            //Doesn't work
+            console.log('-- UPDATE DELIVERY SUCCEEDED --');
+        });
+        this.refreshPage();
+    };
+    GetAllDeliveriesComponent.prototype.refreshPage = function () {
+        var _this = this;
+        //Refresh page in order to add dishes in order-line-components
+        this.sub = this._Activatedroute.paramMap.subscribe(function (params) {
+            var url = 'delivererAdminMenu-ekaly';
+            _this._router
+                .navigateByUrl('/', {
+                skipLocationChange: true
+            })
+                .then(function () {
+                _this._router.navigate([url]);
+            });
         });
     };
     GetAllDeliveriesComponent = __decorate([
--- frontend/src/app/get-all-deliveries/get-all-deliveries.component.ts
+++ frontend/src/app/get-all-deliveries/get-all-deliveries.component.ts
@@ -1,27 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 import { DeliveryService } from '../shared/delivery.service';
+import { Router, ActivatedRoute } from '@angular/router';
+
 import { Delivery } from '../shared/delivery.model';
-
-
 
 @Component({
   selector: 'app-get-all-deliveries',
   templateUrl: './get-all-deliveries.component.html',
-  styleUrls: ['./get-all-deliveries.component.css']
+  styleUrls: ['./get-all-deliveries.component.css'],
 })
 export class GetAllDeliveriesComponent implements OnInit {
-
-  constructor(public deliveryService: DeliveryService) { }
+  IdDeliverer = localStorage.getItem('IdDeliverer');
+  newStrIdDeliverer!: any;
+  constructor(
+    private _Activatedroute: ActivatedRoute,
+    public deliveryService: DeliveryService,
+    private _router: Router
+  ) {}
+  sub: any;
 
   ngOnInit(): void {
     this.GetOrderDeliveryList();
   }
 
-  GetOrderDeliveryList(){
+  GetOrderDeliveryList() {
     this.deliveryService.GetOrderDeliveryList().subscribe((res) => {
       this.deliveryService.delivery = res as Delivery[];
-      console.log("DeliveryList =" + this.deliveryService.delivery);
+      console.log('DeliveryList =' + this.deliveryService.delivery);
     });
   }
+  onSubmit(delivery: Delivery) {
+    // Esorina ny []
+    var objIdDeliverer = String(this.IdDeliverer).replace('[', '');
+    var strIdDeliverer = objIdDeliverer.replace('"', '');
+    var lastRemovedCharStrIdDeliverer = strIdDeliverer.replace(']', '');
+    this.newStrIdDeliverer = lastRemovedCharStrIdDeliverer.replace('"', '');
+    var data = {
+      delivery_deliverer: this.newStrIdDeliverer,
+    };
+    this.deliveryService
+      .putDelivery(data, String(delivery._id))
+      .subscribe((res) => {
+        //Doesn't work
+        console.log('-- UPDATE DELIVERY SUCCEEDED --');
+      });
+      this.refreshPage();
+  }
 
+  refreshPage() {
+    //Refresh page in order to add dishes in order-line-components
+    this.sub = this._Activatedroute.paramMap.subscribe((params) => {
+      const url = 'delivererAdminMenu-ekaly';
+      this._router
+        .navigateByUrl('/', {
+          skipLocationChange: true,
+        })
+        .then(() => {
+          this._router.navigate([url]);
+        });
+    });
+  }
 }
